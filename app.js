edec132f34d750d3028ccf41cbf7386191d5494e
aceaalina <acea.alina@gmail.com>
2024-07-21 19:34:34 +0300

    feat: add functionality to display weather forecast for the next days

--- app.js
+++ app.js
@@ -1,29 +1,98 @@
 const showWeatherBtn = document.getElementById("show-weather");
 const cityInput = document.getElementById("city");
 const weatherContainer = document.getElementById("weather-container");
+const weatherDays = document.getElementById("weather-days");
 
-showWeatherBtn.addEventListener("click", showWeather);
+showWeatherBtn.addEventListener("click", getData);
 
 const URL_CURRENT_WEATHER =
   "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
+const URL_FORECAST_WEATHER =
+  "https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
 
-async function showWeather() {
-  const city = cityInput.value;
+async function getCurrentWeather(city) {
   const response = await fetch(`${URL_CURRENT_WEATHER}${city}`);
-  const weather = await response.json();
+  const curentWeather = await response.json();
+  return curentWeather;
+}
 
-  const iconCode = weather.weather[0].icon;
+async function getForecastWeather(city) {
+  const response = await fetch(`${URL_FORECAST_WEATHER}${city}`);
+  const forecastWeather = await response.json();
+  return forecastWeather;
+}
+
+async function getData() {
+  const city = cityInput.value;
+  const curentWeather = await getCurrentWeather(city);
+
+  showWeather(curentWeather);
+
+  const forecastWeather = await getForecastWeather(city);
+
+  showWeekWeather(forecastWeather);
+}
+
+function getNextWeekDays() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date();
+  let weekDays = [];
+
+  for (let i = 0; i < 5; i++) {
+    let dayIndex = (today.getDay() + i) % 5;
+    weekDays.push(daysOfWeek[dayIndex]);
+  }
+
+  return weekDays;
+}
+
+function showWeekWeather(weatherDate) {
+  const nextWeekDays = getNextWeekDays();
+  let cardsList = " ";
+
+  for (let i = 0; i < nextWeekDays.length; i++) {
+    const curentWeather = weatherDate.list[i * 8];
+    console.log(curentWeather);
+    const iconCode = curentWeather.weather[0].icon;
+    const iconImageUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+
+    cardsList += `
+      <div id="card-${i}">
+         <p> ${nextWeekDays[i]}</p>
+         <img src="${iconImageUrl}"/>
+         <p> ${curentWeather.weather[0].main}</p>
+         <p> ${curentWeather.main.temp_min}</p>
+          <p> ${curentWeather.main.temp_max}</p>
+           <p> ${curentWeather.main.humidity}</p>
+            <p> ${curentWeather.main.feels_like}</p>
+          
+      </div>
+    `;
+  }
+  weatherDays.innerHTML = cardsList;
+}
+
+function showWeather(weatherDate) {
+  const iconCode = weatherDate.weather[0].icon;
   const iconImageUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
 
   weatherContainer.innerHTML = `
   <div>
-    <img src=${iconImageUrl}/>
-    <p> Descriere: ${weather.weather[0].description}</p>
-    <p>Umiditate: ${weather.main.humidity}</p>
-    <p>Presiune: ${weather.main.pressure}</p>
-    <p>Temperatura curenta: ${weather.main.temp}</p>
-    <p>Maxima zilei: ${weather.main.temp_max} </p>
-    <p>Minima zilei: ${weather.main.temp_min}</p>
+    <img src="${iconImageUrl}"/>
+    <p>Descriere: ${weatherDate.weather[0].description}</p>
+    <p>Umiditate: ${weatherDate.main.humidity}</p>
+    <p>Presiune: ${weatherDate.main.pressure}</p>
+    <p class="temperatura">Temperatura curenta: ${weatherDate.main.temp}</p>
+    <p>Maxima zilei: ${weatherDate.main.temp_max} </p>
+    <p>Minima zilei: ${weatherDate.main.temp_min}</p>
   </div>
   `;
 }
